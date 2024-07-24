16edf294d77dd3f613f638829f537e70db4e3861
Alan Jian <alanjian85@outlook.com>
2024-07-24 17:36:31 +0800

    feat: add linker script

--- src/init.c
+++ src/init.c
@@ -2,18 +2,17 @@
 #include <errno.h>
 #include <proc.h>
 #include <reboot.h>
+#include <shell.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 
-void start();
-
-void init(void) {
+void main(void) {
     printf("Booting up CargOS v%d.%d.%d...\n", VERSION_MAJOR, VERSION_MINOR,
            VERSION_PATCH);
 
     const char *argv[] = {"shell", nullptr};
-    proc(start, argv);
+    proc(shell, argv);
 
     while (true) {
         if (wait(nullptr) < 0 && errno == ECHILD) {
--- src/main.c
+++
@@ -1,6 +0,0 @@
-#include <shell.h>
-
-int main() {
-    shell();
-    return 0;
-}
